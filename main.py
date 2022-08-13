a75233b092476709c98918e4656602048b9020a5
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-13 17:49:48 -0400

    Added random.choice for pleas and challenges

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from secrets import choice
 import my_hero
 import villain_one
 import villain_two
@@ -25,13 +26,13 @@ print_slow(f'Level: {villain_one.villain_one['level']} Health: {villain_one.vill
 print('')
 
 while villain_one.villian_one['health'] !=0:
-    print_slow(f'{villain_one.villian_one['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random {villain_one.villain_one['attacks'][]}'!!')
+    print_slow(f'{villain_one.villian_one['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random.choice(villain_one.villain_one['attacks'])'!!')
     print('')
-    print_slow('\033[1;31;40m'+ {villain_one.villian_one['name']}':' + random {villain_one.villain_one['challenges']})
+    print_slow('\033[1;31;40m'+ {villain_one.villian_one['name']}':' + random.choice(villain_one.villain_one['challenges']))
     print('')
     print_slow(f'{my_hero.hero['name']} has {my_hero.hero['health']-villain_one.villain_one['attacks'][3][1]} remaining.')
     print('')
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['pleas']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['pleas']))
     print('')
     print_slow('CHOOSE YOUR ATTACK:')
     print('')
@@ -45,7 +46,7 @@ while villain_one.villian_one['health'] !=0:
     print('')
     user_attack =input('SELECT YOUR ATTACK: ')
     
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['challenges']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['challenges']))
     print('')
     if user_attack == 0:
         print_slow(f'{my_hero.hero['name']} has attacked {villain_one.villain_one['name']} with {my_hero.hero['attacks'][0][0]} and caused {my_hero.hero['attacks'][0][1]} damage!!')
@@ -71,7 +72,7 @@ while villain_one.villian_one['health'] !=0:
     if my_hero.hero['health'] ==0:
         print_slow(f'{my_hero.hero['name']} HAS BEEN DEFEATED BY {villain_one.villain_one['name']}!! GAME OVER!!!')
     if villain_one.villian_one['health'] ==0:
-        print_slow('\033[1;31;40m'+ {villain_one.villian_one['name']}':' + random {villain_one.villain_one['pleas']})
+        print_slow('\033[1;31;40m'+ {villain_one.villian_one['name']}':' + random.choice(villain_one.villain_one['pleas']))
         print('')
         print_slow(f'{my_hero.hero['name']} HAS DEFEATED {villain_one.villain_one['name']}!! TIME TO LEVEL UP!')
         print('')
@@ -82,7 +83,9 @@ while villain_one.villian_one['health'] !=0:
         my_hero.hero['coins'] = my_hero.hero['coins']['copper']['silver']['gold']+villain_one.villian_one['coins']['copper']['silver']['gold']
         print_slow(f'{my_hero.hero['name']} now has {myhero.hero['coins']['copper']',' +my_hero.hero['coins']['silver']} silver, and' +my_hero.hero['coins']['gold']} 'gold coins.')
         print('')
-        my_hero.hero['equipment'] = my_hero.hero['equipment'] + villain_one.villian_one['equipment']
+        my_converted_equipment_set = list(my_hero.hero['equipment'])
+        my_converted_equipment_set.append = villain_one.villian_one['equipment']
+        my_hero.hero['equipment'] = set(my_converted_equipment_set)
         print_slow(f'\033[1;33;40m + Added {villain_one.villian_one['equipment']} equipment!')
         print('')
         my_hero.hero['level'] = my_hero.hero['level']+1
@@ -108,13 +111,13 @@ print_slow(f'Level: {villain_two.villain_two['level']} Health: {villain_two.vill
 print('')
 
 while villain_two.villian_two['health'] !=0:
-    print_slow(f'{villain_two.villian_two['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random {villain_two.villain_two['attacks'][]}'!!')
+    print_slow(f'{villain_two.villian_two['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random.choice(villain_two.villain_two['attacks'])'!!')
     print('')
-    print_slow('\033[1;31;40m'+ {villain_two.villian_two['name']}':' + random {villain_two.villain_two['challenges']})
+    print_slow('\033[1;31;40m'+ {villain_two.villian_two['name']}':' + random.choice(villain_two.villain_two['challenges']))
     print('')
     print_slow(f'{my_hero.hero['name']} has {my_hero.hero['health']-villain_two.villain_two['attacks'][3][1]} remaining.')
     print('')
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['pleas']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['pleas']))
     print('')
     print_slow('CHOOSE YOUR ATTACK:')
     print('')
@@ -128,7 +131,7 @@ while villain_two.villian_two['health'] !=0:
     print('')
     user_attack =input('SELECT YOUR ATTACK: ')
     
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['challenges']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['challenges']))
     print('')
     if user_attack == 0:
         print_slow(f'{my_hero.hero['name']} has attacked {villain_two.villain_two['name']} with {my_hero.hero['attacks'][0][0]} and caused {my_hero.hero['attacks'][0][1]} damage!!')
@@ -154,7 +157,7 @@ while villain_two.villian_two['health'] !=0:
     if my_hero.hero['health'] ==0:
         print_slow(f'{my_hero.hero['name']} HAS BEEN DEFEATED BY {villain_two.villain_two['name']}!! GAME OVER!!!')
     if villain_two.villian_two['health'] ==0:
-        print_slow('\033[1;31;40m'+ {villain_two.villian_two['name']}':' + random {villain_two.villain_two['pleas']})
+        print_slow('\033[1;31;40m'+ {villain_two.villian_two['name']}':' + random.choice(villain_two.villain_two['pleas']))
         print('')
         print_slow(f'{my_hero.hero['name']} HAS DEFEATED {villain_two.villain_two['name']}!! TIME TO LEVEL UP!')
         print('')
@@ -165,7 +168,9 @@ while villain_two.villian_two['health'] !=0:
         my_hero.hero['coins'] = my_hero.hero['coins']['copper']['silver']['gold']+villain_two.villian_two['coins']['copper']['silver']['gold']
         print_slow(f'{my_hero.hero['name']} now has {myhero.hero['coins']['copper']',' +my_hero.hero['coins']['silver']} silver, and' +my_hero.hero['coins']['gold']} 'gold coins.')
         print('')
-        my_hero.hero['equipment'] = my_hero.hero['equipment'] + villain_two.villian_two['equipment']
+        my_converted_equipment_set = list(my_hero.hero['equipment'])
+        my_converted_equipment_set.append = villain_two.villian_two['equipment']
+        my_hero.hero['equipment'] = set(my_converted_equipment_set)
         print_slow(f'\033[1;33;40m + Added {villain_two.villian_two['equipment']} equipment!')
         print('')
         my_hero.hero['level'] = my_hero.hero['level']+1
@@ -188,13 +193,13 @@ print_slow(f'Level: {villain_three.villain_three['level']} Health: {villain_thre
 print('')
 
 while villain_three.villian_three['health'] !=0:
-    print_slow(f'{villain_three.villian_three['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random {villain_three.villain_three['attacks'][]}'!!')
+    print_slow(f'{villain_three.villian_three['name']} HAS ATTACKED {my_hero.hero['name']} with ' + random.choice(villain_three.villain_three['attacks'])'!!')
     print('')
-    print_slow('\033[1;31;40m'+ {villain_three.villian_three['name']}':' + random {villain_three.villain_three['challenges']})
+    print_slow('\033[1;31;40m'+ {villain_three.villian_three['name']}':' + random.choice(villain_three.villain_three['challenges']))
     print('')
     print_slow(f'{my_hero.hero['name']} has {my_hero.hero['health']-villain_three.villain_three['attacks'][3][1]} remaining.')
     print('')
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['pleas']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['pleas']))
     print('')
     print_slow('CHOOSE YOUR ATTACK:')
     print('')
@@ -208,7 +213,7 @@ while villain_three.villian_three['health'] !=0:
     print('')
     user_attack =input('SELECT YOUR ATTACK: ')
     
-    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random {my_hero.hero['challenges']})
+    print_slow('\033[1;35;40m'+ {my_hero.hero['name']}':' + random.choice(my_hero.hero['challenges']))
     print('')
     if user_attack == 0:
         print_slow(f'{my_hero.hero['name']} has attacked {villain_three.villain_three['name']} with {my_hero.hero['attacks'][0][0]} and caused {my_hero.hero['attacks'][0][1]} damage!!')
@@ -245,7 +250,9 @@ while villain_three.villian_three['health'] !=0:
          my_hero.hero['coins'] = my_hero.hero['coins']['copper']['silver']['gold']+villain_three.villian_three['coins']['copper']['silver']['gold']
         print_slow(f'{my_hero.hero['name']} now has {myhero.hero['coins']['copper']',' +my_hero.hero['coins']['silver'] 'silver, and ' +my_hero.hero['coins']['gold'] 'gold coins.')
         print('')
-        my_hero.hero['equipment'] = my_hero.hero['equipment'] + villain_three.villian_three['equipment']
+        my_converted_equipment_set = list(my_hero.hero['equipment'])
+        my_converted_equipment_set.append = villain_three.villian_three['equipment']
+        my_hero.hero['equipment'] = set(my_converted_equipment_set)
         print_slow(f'\033[1;33;40m + Added {villain_three.villian_three['equipment']} equipment!')
         print('')
         my_hero.hero['level'] = my_hero.hero['level']+1
